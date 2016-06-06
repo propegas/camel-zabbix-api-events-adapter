bd71c099b9a8262558de8a042afc8947a20f1845
vgoryachev <vgoryachev@at-consulting.ru>
2016-06-06 10:38:23 +0300

    добавление логирования на несколких уровнях (в разные файлы)

--- src/main/java/ru/atc/camel/zabbix/api/events/Main.java
+++ src/main/java/ru/atc/camel/zabbix/api/events/Main.java
@@ -16,6 +16,10 @@ import ru.at_consulting.itsm.event.Event;
 
 import javax.jms.ConnectionFactory;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public final class Main {
 
@@ -23,8 +27,11 @@ public final class Main {
     private static final int CACHE_SIZE = 2500;
     private static final int MAX_FILE_SIZE = 512000;
 
-    private static String activemq_port;
-    private static String activemq_ip;
+    private static String activemqPort;
+    private static String activemqIp;
+
+    private static String source;
+    private static String adaptername;
 
     private Main() {
 
@@ -36,99 +43,121 @@ public final class Main {
         logger.info("Press CTRL+C to terminate the JVM");
 
         if (args.length == 2) {
-            activemq_port = args[1];
-            activemq_ip = args[0];
+            activemqPort = args[1];
+            activemqIp = args[0];
         }
 
-        if (activemq_port == null || "".equals(activemq_port))
-            activemq_port = "61616";
-        if (activemq_ip == null || "".equals(activemq_ip))
-            activemq_ip = "172.20.19.195";
+        if (activemqPort == null || "".equals(activemqPort))
+            activemqPort = "61616";
+        if (activemqIp == null || "".equals(activemqIp))
+            activemqIp = "172.20.19.195";
 
-        logger.info("activemq_ip: " + activemq_ip);
-        logger.info("activemq_port: " + activemq_port);
+        logger.info("activemqIp: " + activemqIp);
+        logger.info("activemqPort: " + activemqPort);
+
+        // get Properties from file
+        Properties prop = new Properties();
+        InputStream input = null;
+        try {
+
+            input = new FileInputStream("zabbixapi.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            source = prop.getProperty("source");
+            adaptername = prop.getProperty("adaptername");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         org.apache.camel.main.Main main = new org.apache.camel.main.Main();
-
-        main.addRouteBuilder(new RouteBuilder() {
-
-            @Override
-            public void configure() throws Exception {
-
-                JsonDataFormat myJson = new JsonDataFormat();
-                myJson.setPrettyPrint(true);
-                myJson.setLibrary(JsonLibrary.Jackson);
-                //myJson.setJsonView(Event.class);
-                myJson.setAllowJmsType(true);
-                myJson.setUnmarshalType(Event.class);
-
-                PropertiesComponent properties = new PropertiesComponent();
-                properties.setLocation("classpath:zabbixapi.properties");
-                getContext().addComponent("properties", properties);
-
-                ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
-                        "tcp://" + activemq_ip + ":" + activemq_port
-                );
-                getContext().addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-
-                File cachefile = new File("sendedEvents.dat");
-                //cachefile.createNewFile();
-
-                // Heartbeats
-                from("timer://foo?period={{heartbeatsdelay}}")
-                        .process(new Processor() {
-                            @Override
-                            public void process(Exchange exchange) throws Exception {
-                                ZabbixAPIConsumer.genHeartbeatMessage(exchange);
-                            }
-                        })
-                        .marshal(myJson)
-                        .to("activemq:{{heartbeatsqueue}}")
-                        .log("*** Heartbeat: ${id}")
-                        .log(LoggingLevel.DEBUG, "***HEARTBEAT BODY: ${in.body}");
-
-                from(new StringBuilder().append("zabbixapi://events?")
-                        .append("delay={{delay}}&")
-                        .append("zabbixapiurl={{zabbixapiurl}}&")
-                        .append("username={{username}}&")
-                        .append("password={{password}}&")
-                        .append("adaptername={{adaptername}}&")
-                        .append("source={{source}}&")
-                        .append("lasteventid={{lasteventid}}&")
-                        .append("zabbixactionprefix={{zabbixactionprefix}}&")
-                        .append("zabbixActionXmlNs={{zabbixactionxmlns}}&")
-                        .append("zabbixtemplatepattern={{zabbixtemplatepattern}}&")
-                        .append("groupCiPattern={{zabbix_group_ci_pattern}}&")
-                        .append("groupSearchPattern={{zabbix_group_search_pattern}}&")
-                        .append("itemCiPattern={{zabbix_item_ke_pattern}}&")
-                        .append("itemCiParentPattern={{zabbix_item_ci_parent_pattern}}&")
-                        .append("itemCiTypePattern={{zabbix_item_ci_type_pattern}}&")
-                        .append("maxEventsPerRequest={{maxEventsPerRequest}}&")
-                        .append("zabbixip={{zabbixip}}")
-                        .toString())
-
-                        .choice()
-                        .when(header("Type").isEqualTo("Error"))
-                        .marshal(myJson)
-                        .to("activemq:{{eventsqueue}}")
-                        .log("Error: ${id} ${header.EventUniqueId}")
-                        .log(LoggingLevel.DEBUG, "*** NEW ERROR BODY: ${in.body}")
-
-                        .otherwise()
-                        .idempotentConsumer(
-                                header("EventUniqueId"),
-                                FileIdempotentRepository.fileIdempotentRepository(
-                                        cachefile, CACHE_SIZE, MAX_FILE_SIZE
-                                )
-                        )
-
-                        .marshal(myJson)
-                        .to("activemq:{{eventsqueue}}")
-                        .log("*** NEW EVENT: ${id} ${header.EventUniqueId}")
-                        .log(LoggingLevel.DEBUG, "*** NEW EVENT BODY: ${in.body}");
-            }
-        });
-
+        main.addRouteBuilder(new IntegrationRoute());
         main.run();
+    }
+
+    public static class IntegrationRoute extends RouteBuilder {
+        @Override
+        public void configure() throws Exception {
+
+            JsonDataFormat myJson = new JsonDataFormat();
+            myJson.setPrettyPrint(true);
+            myJson.setLibrary(JsonLibrary.Jackson);
+            myJson.setAllowJmsType(true);
+            myJson.setUnmarshalType(Event.class);
+
+            PropertiesComponent properties = new PropertiesComponent();
+            properties.setLocation("classpath:zabbixapi.properties");
+            getContext().addComponent("properties", properties);
+
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+                    "tcp://" + activemqIp + ":" + activemqPort
+            );
+            getContext().addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
+            File cachefile = new File("sendedEvents.dat");
+            logger.info(String.format("Cache file created: %s", cachefile.createNewFile()));
+
+            // Heartbeats
+            from("timer://foo?period={{heartbeatsdelay}}")
+                    .process(new Processor() {
+                        @Override
+                        public void process(Exchange exchange) throws Exception {
+                            ZabbixAPIConsumer.genHeartbeatMessage(exchange, adaptername);
+                        }
+                    })
+                    .marshal(myJson)
+                    .to("activemq:{{heartbeatsqueue}}")
+                    .log("*** Heartbeat: ${id}")
+                    .log(LoggingLevel.DEBUG, "***HEARTBEAT BODY: ${in.body}");
+
+            from(new StringBuilder().append("zabbixapi://events?")
+                    .append("delay={{delay}}&")
+                    .append("zabbixapiurl={{zabbixapiurl}}&")
+                    .append("username={{username}}&")
+                    .append("password={{password}}&")
+                    .append("adaptername={{adaptername}}&")
+                    .append("source={{source}}&")
+                    .append("lasteventid={{lasteventid}}&")
+                    .append("zabbixactionprefix={{zabbixactionprefix}}&")
+                    .append("zabbixActionXmlNs={{zabbixactionxmlns}}&")
+                    .append("zabbixtemplatepattern={{zabbixtemplatepattern}}&")
+                    .append("groupCiPattern={{zabbix_group_ci_pattern}}&")
+                    .append("groupSearchPattern={{zabbix_group_search_pattern}}&")
+                    .append("itemCiPattern={{zabbix_item_ke_pattern}}&")
+                    .append("itemCiParentPattern={{zabbix_item_ci_parent_pattern}}&")
+                    .append("itemCiTypePattern={{zabbix_item_ci_type_pattern}}&")
+                    .append("maxEventsPerRequest={{maxEventsPerRequest}}&")
+                    .append("zabbixip={{zabbixip}}")
+                    .toString())
+
+                    .choice()
+                    .when(header("Type").isEqualTo("Error"))
+                    .marshal(myJson)
+                    .to("activemq:{{errorsqueue}}")
+                    .log(LoggingLevel.INFO, "Error: ${id} ${header.EventUniqueId}")
+                    .log(LoggingLevel.INFO, "*** NEW ERROR BODY: ${in.body}")
+
+                    .otherwise()
+                    .idempotentConsumer(
+                            header("EventUniqueId"),
+                            FileIdempotentRepository.fileIdempotentRepository(
+                                    cachefile, CACHE_SIZE, MAX_FILE_SIZE
+                            )
+                    )
+
+                    .marshal(myJson)
+                    .to("activemq:{{eventsqueue}}")
+                    .log("*** NEW EVENT: ${id} ${header.EventUniqueId}")
+                    .log(LoggingLevel.DEBUG, "*** NEW EVENT BODY: ${in.body}");
+        }
     }
 }
